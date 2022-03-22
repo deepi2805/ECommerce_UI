a6edc7348a3db8678d45f8d677a991b0e7397fd6
deepali <dmiital2805@gmail.com>
2022-03-22 22:10:17 +0530

    reuter added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./Login.js"
+import Registration from "./Registration.js"
+import Home from "./Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/"><Login/></Route>
+          <Route exact path="/login"><Login/></Route>
+          <Route exact path="/registration"><Registration/></Route>
+          <Route exact path="/forgotpassword"></Route>
+          <Route exact path="/home"><Home/></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
